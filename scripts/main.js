8cbd53ebe8636fc23f81345fd31dab6aab0c71ff
Nicolas <serna.nicolas.abel@gmail.com>
2024-08-25 02:23:20 -0300

    add script to say hello

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,17 @@ let username, sectionName
 const $ = (s) => document.querySelector(s)
 const $$ = (s) => document.querySelectorAll(s)
 
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+    );
+}
+
 $('#submit-btn')?.addEventListener('click', function (event) {
     event.preventDefault()
     let user = $('#username').value
-    let section = $('#area-selector').value
+    let section = toTitleCase($('#area-selector').value)
 
     sessionStorage.setItem('user', user)
     sessionStorage.setItem('section', section)
