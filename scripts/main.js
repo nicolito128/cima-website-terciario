c5a6e9f17db76aef69de6b7c7023bd2d67c8a0b8
Nicolas <serna.nicolas.abel@gmail.com>
2024-08-25 01:31:09 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+let username, sectionName
+
+const $ = (s) => document.querySelector(s)
+const $$ = (s) => document.querySelectorAll(s)
+
+$('#submit-btn')?.addEventListener('click', function (event) {
+    event.preventDefault()
+    let user = $('#username').value
+    let section = $('#area-selector').value
+
+    sessionStorage.setItem('user', user)
+    sessionStorage.setItem('section', section)
+
+    const link = document.createElement('a')
+    link.href = `/pages/${section}.html`
+    link.click()
+})
+
+$('#userNameSpan')?.value = username
+$('#sectionNameSpan')?.value = sectionName
